9747df737e507024abb6ceec2f783be17b698df1
Dharmesh <raikwardharmesh67@gmail.com>
2024-01-26 23:02:16 +0530

    26 jan

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('lets write some javascript code')
+console.log("lets write some javascript code");
+
+async function getsongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let tds = div.getElementsByTagName("a");
+  let songs = [];
+  for (let i = 0; i < tds.length; i++) {
+    const element = tds[i];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+  return songs;
+}
+async function main() {
+  let song = await getsongs();
+  console.log(song);
+  var audio = new Audio(song[5]);
+//    audio.play();
+   audio.addEventListener("loadeddata", () => {
+    let duration = audio.duration;
+    console.log(duration)
+  });
+}
+main();
